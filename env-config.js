7a527b44026e9168619c89e885819c4275ce3e83
Mike Cann <mike.cann@gmail.com>
2017-12-12 15:04:31 +0800

    using create react app instead

--- env-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  'process.env.ROOT_URL': process.env.ROOT_URL,
-  'process.env.SNIPCART_KEY': process.env.SNIPCART_KEY
-}
--- src/typings/isomorphic-unfetch.d.ts
+++
@@ -1,7 +0,0 @@
-// declare module "isomorphic-unfetch" {
-//     // namespace _fetch { }
-//     // const _fetch: typeof fetch;
-//     // export default _fetch;   
-// }
-
-declare module "isomorphic-unfetch";
