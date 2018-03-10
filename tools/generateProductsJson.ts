4f70891170573af1466d6e7e97e577543ab63daf
Mike Cann <mike.cann@gmail.com>
2018-03-10 19:07:08 +0800

    reverted product gen

--- tools/generateProductsJson.ts
+++ tools/generateProductsJson.ts
@@ -1,12 +1,13 @@
-import { getDb } from "../src/lib/db";
+import {getDb} from "../src/lib/db";
 import * as fs from "fs";
 
-async function run() {
+async function run()
+{
     var db = await getDb();
-
+    
     var products = [];
-    for (var p of db.prints)
-        for (var o of p.printOptions)
+    for(var p of db.prints)
+        for(var o of p.printOptions)
             for (var s of o.sizes)
                 products.push({
                     id: `${p.id}-${o.id}-${s.id}`,
@@ -14,9 +15,7 @@ async function run() {
                     url: "/products.json"
                 })
 
-    const path = `${__dirname}/../../public/products.json`;
-    fs.writeFileSync(path, JSON.stringify(products, null, 2));
-    console.log(`Projects.json written to: ${path}`)
+    fs.writeFileSync(`./public/products.json`, JSON.stringify(products,null, 2));    
 }
 
 try {
