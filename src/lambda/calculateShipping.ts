9fd3d8ba0a86900abc69e6135e57913be44836d4
Mike Cann <mike.cann@gmail.com>
2018-04-06 11:12:28 +0800

    still trying to get te output

--- src/lambda/calculateShipping.ts
+++ src/lambda/calculateShipping.ts
@@ -1,8 +1,7 @@
 
 
 export function handler(event:any, context:any, callback:any) {
-    var str = JSON.stringify({event, context}, null, 2);
-    console.log(str);
+    console.log(event);
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({msg: "Calculated!!"})
