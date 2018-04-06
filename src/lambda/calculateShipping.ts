911ab85f2f4c2270c6b5d28ed0e5029b914f1870
Mike Cann <mike.cann@gmail.com>
2018-04-06 11:33:53 +0800

    fixed compile errors

--- src/lambda/calculateShipping.ts
+++ src/lambda/calculateShipping.ts
@@ -1,16 +1,104 @@
-import { LambdaEvent } from "./types";
+import { LambdaEvent, LambdaCallback } from "./types";
 
-type HTTPBody = {
-
+const exampleBody = {
+  "eventName": "shippingrates.fetch",
+  "mode": "Live",
+  "createdOn": "2015-02-21T14:58:02.6738454Z",
+  "content": {
+    "token": "22808196-0eff-4a6e-b136-3e4d628b3cf5",
+    "creationDate": "2015-02-21T14:58:02.6738454Z",
+    "modificationDate": "2015-02-21T14:58:02.6738454Z",
+    "status": "Processed",
+    "currency": "USD",
+    "paymentMethod": "CreditCard",
+    "email": "customer@snipcart.com",
+    "cardHolderName": "Nicolas Cage",
+    "billingAddressName": "Nicolas Cage",
+    "billingAddressCompanyName": "Company name",
+    "billingAddressAddress1": "888 The street",
+    "billingAddressAddress2": "",
+    "billingAddressCity": "QuÃ©bec",
+    "billingAddressCountry": "CA",
+    "billingAddressProvince": "QC",
+    "billingAddressPostalCode": "G1G 1G1",
+    "billingAddressPhone": "(888) 888-8888",
+    "shippingAddressName": "Nicolas Cage",
+    "shippingAddressCompanyName": "Company name",
+    "shippingAddressAddress1": "888 The street",
+    "shippingAddressAddress2": "",
+    "shippingAddressCity": "QuÃ©bec",
+    "shippingAddressCountry": "CA",
+    "shippingAddressProvince": "QC",
+    "shippingAddressPostalCode": "G1G 1G1",
+    "shippingAddressPhone": "(888) 888-8888",
+    "shippingAddressSameAsBilling": true,
+    "finalGrandTotal": 310.00,
+    "shippingAddressComplete": true,
+    "creditCardLast4Digits": "4242",
+    "shippingFees": 10.00,
+    "shippingMethod": "Livraison",
+    "items": [{
+      "uniqueId": "eb4c9dae-e725-4dad-b7ae-a5e48097c831",
+      "token": "22808196-0eff-4a6e-b136-3e4d628b3cf5",
+      "id": "1",
+      "name": "Movie",
+      "price": 300.00,
+      "originalPrice": 300.00,
+      "quantity": 1,
+      "url": "https://snipcart.com",
+      "weight": 10.00,
+      "description": "Something",
+      "image": "http://placecage.com/50/50",
+      "customFieldsJson": "[]",
+      "stackable": true,
+      "maxQuantity": null,
+      "totalPrice": 300.0000,
+      "totalWeight": 10.00
+    }],
+    "subtotal": 610.0000,
+    "totalWeight": 20.00,
+    "discounts": [],
+    "willBePaidLater": false
+  }
 }
 
-export function handler(event:LambdaEvent<HTTPBody>, context:any, callback:any) {
-    console.log("event", event);
-    console.log("body", event.body)
-    delete event.body;
-    console.log("no body", event)
+type HTTPBody = typeof exampleBody;
+
+const exampleResponse = {
+  "rates": [{
+    "cost": 10,
+    "description": "10$ shipping"
+    }, {
+    "cost": 20,
+    "description": "20$ shipping",
+    "guaranteedDaysToDelivery": 5
+    },
+  ]
+}
+
+type SuccessResponse = typeof exampleResponse;
+
+// const exampleError = {
+//   "errors": [{
+//     "key": "invalid_postal_code",
+//     "message": "The postal code is invalid."
+//     },
+//   ]
+// };
+
+// type ErrorResponse = typeof exampleError;
+
+export function handler(event: LambdaEvent<HTTPBody>, context:any, callback: LambdaCallback) {
+
+    const response : SuccessResponse = {
+      rates: [
+        { cost: 33, description: "33 bucks shipping" }
+      ]
+    }
+
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({msg: "Calculated!!"})
+      body: JSON.stringify(response)
     })
+
   }
