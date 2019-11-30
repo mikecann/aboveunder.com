a324f6b84b48e7e49cecf482e95c83420092c63b
Mike Cann <mike.cann@gmail.com>
2019-11-30 15:44:44 +0800

    fixed compile issue

--- tools/importWPProducts.ts
+++ tools/importWPProducts.ts
@@ -1,122 +1,106 @@
-import * as csvtojson from "csvtojson";
+import csvtojson from "csvtojson";
 import * as fs from "fs";
 import * as path from "path";
 import * as request from "request";
 import { IPrint } from "../src/lib/types";
 //import * as sharp from 'sharp';
-import Jimp =  require('jimp')
-import * as moment from "moment";
+import Jimp from "jimp";
+import moment from "moment";
 
-async function run() 
-{
-    const csvPath = `${__dirname}/../tools/data/wpProducts.csv`;
-    const tsPath = `${__dirname}/../src/lib/printProducts.ts`;
-    var products : Partial<IPrint>[] = [];
+async function run() {
+  const csvPath = `${__dirname}/../tools/data/wpProducts.csv`;
+  const tsPath = `${__dirname}/../src/lib/printProducts.ts`;
+  var products: Partial<IPrint>[] = [];
 
-    console.log(`Importing CSV from: ${csvPath}`);
-    csvtojson()
-        .fromFile(csvPath)
-        .on('json', (json:any) => {
+  console.log(`Importing CSV from: ${csvPath}`);
+  csvtojson()
+    .fromFile(csvPath)
+    .on("json", (json: any) => {
+      var product: Partial<IPrint> = {
+        title: (json.post_title as string).substr(9),
+        image: json.image,
+        description: json.post_excerpt,
+        featured: json._featured == "yes",
+        dateCreated: json.post_date,
+      };
 
-            var product : Partial<IPrint> = {
-                title: (json.post_title as string).substr(9),
-                image: json.image,
-                description: json.post_excerpt,
-                featured: json._featured == "yes",
-                dateCreated: json.post_date
-            };
-
-            products.push(product);
-        })
-        .on('done', async (error:any)=>{
-
-            try
-            {
-                var i = 0;
-                const step = 3;
-    
-                while(i<products.length)
-                {
-                    await loadProductImagesParallel(products.slice(i, i+step)); 
-                    i += step;
-                }
-    
-                console.log(`Generating ${products.length} thumnails..`)
-                
-                for(var product of products)
-                    await generateThumbnail(product)
-
-                products = products.sort((a,b) => moment(a.dateCreated).diff(moment(b.dateCreated).utc()));
-                
-                if (error)
-                    throw error;
-
-                fs.writeFileSync(tsPath, `export const data = ${JSON.stringify(products, null, 2)}`)
-            }
-            catch(e)
-            {
-                console.error("Whoops, error!", e);
-            }
-        });
-}
-
-async function loadProductImagesParallel(products:Partial<IPrint>[])
-{
-    await Promise.all(products.map(p => loadProductImage(p)))
-}
-
-async function generateThumbnail(product:Partial<IPrint>) : Promise<void>
-{
-    const fname = path.basename(product.image + "");
-    const fullPath = `${__dirname}/..${product.image}`;
-    const thumbPath = `${__dirname}/../images/products/thumb/${fname}`;
-
-    if (!fs.existsSync(thumbPath))
-    {
-        console.log("Generating thumb for: ",product);
-
-        var img = await Jimp.read(fullPath);
-        img.resize(800, Jimp.AUTO);
-        img.quality(65);
-        await writeImage(img, thumbPath);
-    }
-
-    product.thumb = `/images/products/thumb/${fname}`;
-}
-
-async function writeImage(img:Jimp, path:string) : Promise<any>
-{
-    return new Promise((resolve,reject) => {
-        console.log("writing..")
-        img.write(path, err => {
-            if (err)
-                reject(err);
-            else
-                resolve();
-        });
+      products.push(product);
     })
-}
+    .on("done", async (error: any) => {
+      try {
+        var i = 0;
+        const step = 3;
 
-async function loadProductImage(product:Partial<IPrint>)
-{
-    const fname = path.basename(product.image + "");
-    const fpath = `static/images/products/full/${fname}`;
-    if (!fs.existsSync(fpath))
-    {
-        console.log('Downloading.. ', fname);    
-        await downloadImg(`https://aboveunder.com${product.image}`, fpath);        
-    }
-    product.image = `/${fpath}`;
-}
+        while (i < products.length) {
+          await loadProductImagesParallel(products.slice(i, i + step));
+          i += step;
+        }
 
-function downloadImg(uri:string, filename:string) : Promise<any> {
+        console.log(`Generating ${products.length} thumnails..`);
 
-    return new Promise<any>((resolve,reject) => {
-        request.head(uri, function(err:any, res:any, body:any){   
-            console.log("Downloading ", {uri})
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', () => resolve());
-          });
+        for (var product of products) await generateThumbnail(product);
+
+        products = products.sort((a, b) => moment(a.dateCreated).diff(moment(b.dateCreated).utc()));
+
+        if (error) throw error;
+
+        fs.writeFileSync(tsPath, `export const data = ${JSON.stringify(products, null, 2)}`);
+      } catch (e) {
+        console.error("Whoops, error!", e);
+      }
     });
-  };
+}
+
+async function loadProductImagesParallel(products: Partial<IPrint>[]) {
+  await Promise.all(products.map(p => loadProductImage(p)));
+}
+
+async function generateThumbnail(product: Partial<IPrint>): Promise<void> {
+  const fname = path.basename(product.image + "");
+  const fullPath = `${__dirname}/..${product.image}`;
+  const thumbPath = `${__dirname}/../images/products/thumb/${fname}`;
+
+  if (!fs.existsSync(thumbPath)) {
+    console.log("Generating thumb for: ", product);
+
+    var img = await Jimp.read(fullPath);
+    img.resize(800, Jimp.AUTO);
+    img.quality(65);
+    await writeImage(img, thumbPath);
+  }
+
+  product.thumb = `/images/products/thumb/${fname}`;
+}
+
+async function writeImage(img: Jimp, path: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    console.log("writing..");
+    img.write(path, err => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+async function loadProductImage(product: Partial<IPrint>) {
+  const fname = path.basename(product.image + "");
+  const fpath = `static/images/products/full/${fname}`;
+  if (!fs.existsSync(fpath)) {
+    console.log("Downloading.. ", fname);
+    await downloadImg(`https://aboveunder.com${product.image}`, fpath);
+  }
+  product.image = `/${fpath}`;
+}
+
+function downloadImg(uri: string, filename: string): Promise<any> {
+  return new Promise<any>((resolve, reject) => {
+    request.head(uri, function(err: any, res: any, body: any) {
+      console.log("Downloading ", { uri });
+      request(uri)
+        .pipe(fs.createWriteStream(filename))
+        .on("close", () => resolve());
+    });
+  });
+}
 
 run();
--- tools/utils.ts
+++ tools/utils.ts
@@ -1,252 +1,254 @@
 import * as fs from "fs";
-import { promisify } from 'util';
+import { promisify } from "util";
 import * as readline from "readline";
 import { getDb } from "../src/lib/db";
-import { IPrint, IDB } from '../src/lib/types';
-import { sortByLatestFirst, sortByOldestFirst, getDirectories } from '../src/lib/utils';
+import { IPrint, IDB } from "../src/lib/types";
+import { sortByLatestFirst, sortByOldestFirst, getDirectories } from "../src/lib/utils";
 import * as path from "path";
-import Jimp =  require('jimp')
+import Jimp from "jimp";
 import { ExifImage } from "exif";
-import * as moment from "moment";
+import moment from "moment";
 import { CollectionImage } from "./CollectionImage";
 
 var exampleExifData = {
-    image:
-        {
-            Make: 'DJI',
-            Model: 'FC220',
-            XResolution: 240,
-            YResolution: 240,
-            ResolutionUnit: 2,
-            Software: 'Adobe Photoshop Lightroom Classic 7.1 (Windows)',
-            ModifyDate: '2018:02:25 17:08:41',
-            ExifOffset: 212,
-            GPSInfo: 808
-        },
-    thumbnail:
-        {
-            Compression: 6,
-            XResolution: 72,
-            YResolution: 72,
-            ResolutionUnit: 2,
-            ThumbnailOffset: 1036,
-            ThumbnailLength: 24662
-        },
-    exif:
-        {
-            ExposureTime: 0.0004347826086956522,
-            FNumber: 2.2,
-            ExposureProgram: 58655,
-            ISO: 100,
-            ExifVersion: null,
-            DateTimeOriginal: '2017:06:23 11:10:02',
-            CreateDate: '2017:06:23 11:10:02',
-            ShutterSpeedValue: 11.167418,
-            ApertureValue: 2.275007,
-            ExposureCompensation: -1.3125,
-            MaxApertureValue: 2.27,
-            SubjectDistance: 0,
-            MeteringMode: 2,
-            LightSource: 0,
-            Flash: 32,
-            FocalLength: 4.7,
-            SubSecTimeOriginal: '1',
-            SubSecTimeDigitized: '1',
-            ColorSpace: 1,
-            FileSource: null,
-            SceneType: null,
-            CustomRendered: 0,
-            ExposureMode: 2,
-            WhiteBalance: 0,
-            DigitalZoomRatio: 0,
-            FocalLengthIn35mmFormat: 26,
-            SceneCaptureType: 0,
-            GainControl: 0,
-            Contrast: 0,
-            Saturation: 0,
-            Sharpness: 0,
-            SubjectDistanceRange: 0,
-            SerialNumber: '2014031100',
-            LensInfo: [26.3, 26.3, 2.2, 2.2],
-            LensModel: '26.3 mm f/2.2'
-        },
-    gps:
-        {
-            GPSVersionID: [2, 3, 0, 0],
-            GPSLatitudeRef: 'S',
-            GPSLatitude: [19, 7.4, 0],
-            GPSLongitudeRef: 'E',
-            GPSLongitude: [146, 52.6667, 0],
-            GPSAltitude: 7.9
-        },
-    interoperability: {},
-    makernote: {}
+  image: {
+    Make: "DJI",
+    Model: "FC220",
+    XResolution: 240,
+    YResolution: 240,
+    ResolutionUnit: 2,
+    Software: "Adobe Photoshop Lightroom Classic 7.1 (Windows)",
+    ModifyDate: "2018:02:25 17:08:41",
+    ExifOffset: 212,
+    GPSInfo: 808,
+  },
+  thumbnail: {
+    Compression: 6,
+    XResolution: 72,
+    YResolution: 72,
+    ResolutionUnit: 2,
+    ThumbnailOffset: 1036,
+    ThumbnailLength: 24662,
+  },
+  exif: {
+    ExposureTime: 0.0004347826086956522,
+    FNumber: 2.2,
+    ExposureProgram: 58655,
+    ISO: 100,
+    ExifVersion: null,
+    DateTimeOriginal: "2017:06:23 11:10:02",
+    CreateDate: "2017:06:23 11:10:02",
+    ShutterSpeedValue: 11.167418,
+    ApertureValue: 2.275007,
+    ExposureCompensation: -1.3125,
+    MaxApertureValue: 2.27,
+    SubjectDistance: 0,
+    MeteringMode: 2,
+    LightSource: 0,
+    Flash: 32,
+    FocalLength: 4.7,
+    SubSecTimeOriginal: "1",
+    SubSecTimeDigitized: "1",
+    ColorSpace: 1,
+    FileSource: null,
+    SceneType: null,
+    CustomRendered: 0,
+    ExposureMode: 2,
+    WhiteBalance: 0,
+    DigitalZoomRatio: 0,
+    FocalLengthIn35mmFormat: 26,
+    SceneCaptureType: 0,
+    GainControl: 0,
+    Contrast: 0,
+    Saturation: 0,
+    Sharpness: 0,
+    SubjectDistanceRange: 0,
+    SerialNumber: "2014031100",
+    LensInfo: [26.3, 26.3, 2.2, 2.2],
+    LensModel: "26.3 mm f/2.2",
+  },
+  gps: {
+    GPSVersionID: [2, 3, 0, 0],
+    GPSLatitudeRef: "S",
+    GPSLatitude: [19, 7.4, 0],
+    GPSLongitudeRef: "E",
+    GPSLongitude: [146, 52.6667, 0],
+    GPSAltitude: 7.9,
+  },
+  interoperability: {},
+  makernote: {},
 };
 
 export type ExifData = typeof exampleExifData;
 
 export async function getFilesInDir(dir: string): Promise<string[]> {
-    return await (promisify(fs.readdir))(dir);
+  return await promisify(fs.readdir)(dir);
 }
 
 export async function askQuestion(question: string, defaultVal?: string): Promise<string> {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  var q = defaultVal ? `${question} (${defaultVal})` : question;
+
+  return new Promise<string>((resolve, reject) => {
+    rl.question(q + " ", answer => {
+      if (!answer && defaultVal) answer = defaultVal;
+
+      rl.close();
+      resolve(answer);
     });
-
-    var q = defaultVal ? `${question} (${defaultVal})` : question;
-
-    return new Promise<string>((resolve, reject) => {
-        rl.question(q + " ", answer => {
-            if (!answer && defaultVal)
-                answer = defaultVal;
-
-            rl.close();
-            resolve(answer);
-        })
-    })
+  });
 }
 
 export async function areEqual(filePath1: string, filePath2: string): Promise<boolean> {
-    var a = await promisify(fs.readFile)(filePath1);
-    var b = await promisify(fs.readFile)(filePath2);
-    return a.equals(b);
+  var a = await promisify(fs.readFile)(filePath1);
+  var b = await promisify(fs.readFile)(filePath2);
+  return a.equals(b);
 }
 
 export async function askConfirmation(question: string): Promise<boolean> {
-    var answer = await askQuestion(`${question} (Y/n)`);
-    if (!answer)
-        return true;
+  var answer = await askQuestion(`${question} (Y/n)`);
+  if (!answer) return true;
 
-    return answer.toLowerCase() == "y";
+  return answer.toLowerCase() == "y";
 }
 
 export async function renameFile(from: string, to: string): Promise<void> {
-    await (promisify(fs.rename))(from, to);
+  await promisify(fs.rename)(from, to);
 }
 
 export async function copyFile(from: string, to: string): Promise<void> {
-    await (promisify(fs.copyFile))(from, to);
+  await promisify(fs.copyFile)(from, to);
 }
 
 export async function saveDbPrints(prints: IPrint[]): Promise<void> {
-    const tsPath = `./src/lib/printProducts.ts`;
-    prints = sortByOldestFirst(prints.slice());
-    for (var p of prints) {
-        delete p.printOptions;
-        delete p.id;
-    }
-    await fs.writeFileSync(tsPath, `export const data = ${JSON.stringify(prints, null, 2)}`);
+  const tsPath = `./src/lib/printProducts.ts`;
+  prints = sortByOldestFirst(prints.slice());
+  for (var p of prints) {
+    delete p.printOptions;
+    delete p.id;
+  }
+  await fs.writeFileSync(tsPath, `export const data = ${JSON.stringify(prints, null, 2)}`);
 }
 
-export async function generateThumbnail(product: Partial<IPrint>, size: number = 600, quality: number = 65): Promise<void> {
-    const fname = path.basename(product.image + "");
-    const fullPath = `./public${product.image}`;
-    const thumbPath = `./public/images/products/thumb/${fname}`;
+export async function generateThumbnail(
+  product: Partial<IPrint>,
+  size: number = 600,
+  quality: number = 65
+): Promise<void> {
+  const fname = path.basename(product.image + "");
+  const fullPath = `./public${product.image}`;
+  const thumbPath = `./public/images/products/thumb/${fname}`;
 
-    console.log("Generating thumb for: ", product.title);
+  console.log("Generating thumb for: ", product.title);
 
-    var img = await Jimp.read(fullPath);
-    img.resize(size, Jimp.AUTO);
-    img.quality(quality);
-    await writeImage(img, thumbPath);
+  var img = await Jimp.read(fullPath);
+  img.resize(size, Jimp.AUTO);
+  img.quality(quality);
+  await writeImage(img, thumbPath);
 }
 
-export async function resizeImg(srcPath: string, destPath: string, width: number, quality: number = 65): Promise<void> {
-    console.log(`Resizing image '${srcPath}' to '${destPath}' with width ${width} and quality ${quality}`);
-    var img = await Jimp.read(srcPath);
-    img.resize(width, Jimp.AUTO);
-    img.quality(quality);
-    await writeImage(img, destPath);
+export async function resizeImg(
+  srcPath: string,
+  destPath: string,
+  width: number,
+  quality: number = 65
+): Promise<void> {
+  console.log(
+    `Resizing image '${srcPath}' to '${destPath}' with width ${width} and quality ${quality}`
+  );
+  var img = await Jimp.read(srcPath);
+  img.resize(width, Jimp.AUTO);
+  img.quality(quality);
+  await writeImage(img, destPath);
 }
 
-export async function limitImageSize(path: string, maxSize: number, quality: number = 65): Promise<void> {
-    var img = await Jimp.read(path);
-    if (img.bitmap.width <= maxSize) {
-        console.log(`Image '${path}' is already <= ${maxSize}, skipping`);
-        return;
-    }
+export async function limitImageSize(
+  path: string,
+  maxSize: number,
+  quality: number = 65
+): Promise<void> {
+  var img = await Jimp.read(path);
+  if (img.bitmap.width <= maxSize) {
+    console.log(`Image '${path}' is already <= ${maxSize}, skipping`);
+    return;
+  }
 
-    console.log(`Processing image '${path}' resizing to ${maxSize} with quality ${quality}`);
+  console.log(`Processing image '${path}' resizing to ${maxSize} with quality ${quality}`);
 
-    img.resize(maxSize, Jimp.AUTO);
-    img.quality(quality);
-    await writeImage(img, path);
+  img.resize(maxSize, Jimp.AUTO);
+  img.quality(quality);
+  await writeImage(img, path);
 }
 
 export async function writeImage(img: Jimp, path: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-        img.write(path, err => {
-            if (err)
-                reject(err);
-            else
-                resolve();
-        });
-    })
+  return new Promise((resolve, reject) => {
+    img.write(path, err => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
 }
 
 export async function wait(ms: number): Promise<void> {
-    return new Promise<void>(resolve => setTimeout(resolve, ms));
+  return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
 
 export async function getEXIFData(imgPath: string): Promise<ExifData> {
-    return new Promise<any>((resolve, reject) => {
-        new ExifImage({ image: imgPath }, (err, data) => {
-            if (err)
-                reject(err);
+  return new Promise<any>((resolve, reject) => {
+    new ExifImage({ image: imgPath }, (err, data) => {
+      if (err) reject(err);
 
-            resolve(data);
-        });
+      resolve(data);
     });
+  });
 }
 
 export async function getGPSData(imgPath: string): Promise<google.maps.LatLngLiteral> {
-    return extractLatLngFromExif(await this.getEXIFData(imgPath));
+  return extractLatLngFromExif(await this.getEXIFData(imgPath));
 }
 
 export function extractLatLngFromExif(exif: ExifData): google.maps.LatLngLiteral {
-    if (!exif || !exif.gps || !exif.gps.GPSLatitude)
-        return undefined;
+  if (!exif || !exif.gps || !exif.gps.GPSLatitude) return undefined;
 
-    return {
-        lat: convertDMStoDD(exif.gps.GPSLatitude, exif.gps.GPSLatitudeRef),
-        lng: convertDMStoDD(exif.gps.GPSLongitude, exif.gps.GPSLongitudeRef)
-    }
+  return {
+    lat: convertDMStoDD(exif.gps.GPSLatitude, exif.gps.GPSLatitudeRef),
+    lng: convertDMStoDD(exif.gps.GPSLongitude, exif.gps.GPSLongitudeRef),
+  };
 }
 
-function convertDMStoDD(dms: number[], ref:string) : number
-{
-    const hour = dms[0];
-    const minute = dms[1];
-    const second = dms[2];
-    const negate = ref == 'S' || ref == 'W';
-    return (hour + (minute/60)+(second/(60*60))) * (negate?-1:1);
+function convertDMStoDD(dms: number[], ref: string): number {
+  const hour = dms[0];
+  const minute = dms[1];
+  const second = dms[2];
+  const negate = ref == "S" || ref == "W";
+  return (hour + minute / 60 + second / (60 * 60)) * (negate ? -1 : 1);
 }
 
 export function extractDateCreatedFromExif(exif: ExifData): string {
-    if (!exif || !exif.exif || !exif.exif.CreateDate)
-        return undefined;
+  if (!exif || !exif.exif || !exif.exif.CreateDate) return undefined;
 
-    return moment(exif.exif.CreateDate, "YYYY:MM:DD HH:mm:ss").format();
+  return moment(exif.exif.CreateDate, "YYYY:MM:DD HH:mm:ss").format();
 }
 
-export async function createPrint(image:CollectionImage , db:IDB) : Promise<IPrint>
-{
-    console.log(`For image: ${image.filename}`);
-    
-    const print : IPrint = {
-        id: undefined,
-        printOptions: undefined,
-        image: `/images/products/full/${image.filenameNoSpaces}`,
-        thumb: `/images/products/thumb/${image.filenameNoSpaces}`,
-        title: image.name,
-        dateCreated: moment().format(),
-        description: await askQuestion("Description?", ""),
-        featured: await askConfirmation("Featured?")
-    };
+export async function createPrint(image: CollectionImage, db: IDB): Promise<IPrint> {
+  console.log(`For image: ${image.filename}`);
 
-    db.prints.push(print);
+  const print: IPrint = {
+    id: undefined,
+    printOptions: undefined,
+    image: `/images/products/full/${image.filenameNoSpaces}`,
+    thumb: `/images/products/thumb/${image.filenameNoSpaces}`,
+    title: image.name,
+    dateCreated: moment().format(),
+    description: await askQuestion("Description?", ""),
+    featured: await askConfirmation("Featured?"),
+  };
 
-    return print;
+  db.prints.push(print);
+
+  return print;
 }
